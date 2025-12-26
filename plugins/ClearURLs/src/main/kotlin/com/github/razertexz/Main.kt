42d17d392aa9077329471dc9d81f722246144e0d
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2025-12-26 10:41:09 +0700

    misc: update

--- plugins/ClearURLs/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugins/ClearURLs/src/main/kotlin/com/github/razertexz/Main.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.Utils
+import com.aliucord.Http
 
 import com.discord.utilities.uri.UriHandler
 
@@ -13,20 +14,25 @@ import kotlin.jvm.functions.Function0
 import de.robv.android.xposed.XC_MethodHook
 
 @AliucordPlugin(requiresRestart = false)
-class Main : Plugin() {
+internal class Main : Plugin() {
+    private class Provider(
+        @JvmField val urlPattern: String,
+        @JvmField val rules: Array<String>?,
+        @JvmField val rawRules: Array<String>?,
+        @JvmField val exceptions: Array<String>?,
+        @JvmField val redirections: Array<String>?
+    )
+
     override fun start(ctx: Context) {
+        val rules = Http.simpleJsonGet("https://rules2.clearurls.xyz/data.minify.json", Array<Provider>::class.java)
+
         patcher.patch(UriHandler::class.java, "handle", arrayOf(Context::class.java, String::class.java, Boolean::class.java, Boolean::class.java, Function0::class.java), object : XC_MethodHook(-10000) {
             override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam) {
-                clearUrl(param.args[1] as String)
+                val originalUrl = param.args[1] as String
+                Utils.showToast(rules["adtech"].url)
             }
         })
     }
 
     override fun stop(ctx: Context) = patcher.unpatchAll()
-
-    private fun clearUrl(url: String): String {
-        Utils.showToast(url)
-
-        return ""
-    }
 }
