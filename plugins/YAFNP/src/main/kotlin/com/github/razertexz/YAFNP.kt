5042bb593d4e00204e9cba4dbfc758c8158c77d6
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2026-01-04 06:30:25 +0700

    misc: update

--- plugins/YAFNP/src/main/kotlin/com/github/razertexz/YAFNP.kt
+++ plugins/YAFNP/src/main/kotlin/com/github/razertexz/YAFNP.kt
@@ -5,23 +5,32 @@ import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 
-import com.discord.models.presence.Presence
+import com.discord.api.presence.Presence
 import com.discord.api.activity.Activity
 import com.discord.api.activity.ActivityType
-import com.discord.api.presence.ClientStatus
-import com.discord.api.presence.ClientStatuses
 
 import de.robv.android.xposed.XC_MethodHook
 
 @AliucordPlugin(requiresRestart = false)
 internal class YAFNP : Plugin() {
     override fun start(context: Context) {
-        patcher.patch(Presence::class.java.getDeclaredConstructor(ClientStatus::class.java, ClientStatuses::class.java, List::class.java), object : XC_MethodHook() {
+        val map = HashMap<Long, String>()
+
+        patcher.patch(Presence::class.java.declaredConstructors[0], object : XC_MethodHook() {
             override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
-                val activities = param.args[2] as List<Activity>
+                val activities = param.args[1] as List<Activity>
+                val userId = param.args[4] as Long? ?: return
+
                 for (activity in activities) {
                     if (activity.p() == ActivityType.CUSTOM_STATUS) {
-                        logger.infoToast(activity.toString())
+                        val state = activity.l()
+                        if (state != null) {
+                            val hiddenText = extractHiddenText(state)
+                            if (hiddenText.isNotEmpty()) {
+                                map[userId] = hiddenText
+                            }
+                        }
+
                         break
                     }
                 }
