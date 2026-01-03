a722d63c473fa15e70130e60942c2e45da31741d
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2026-01-03 22:27:13 +0700

    misc: update

--- plugins/YAFNP/src/main/kotlin/com/github/razertexz/YAFNP.kt
+++ plugins/YAFNP/src/main/kotlin/com/github/razertexz/YAFNP.kt
@@ -8,15 +8,13 @@ import com.aliucord.entities.Plugin
 import com.discord.models.presence.Presence
 import com.discord.api.activity.Activity
 import com.discord.api.activity.ActivityType
-import com.discord.api.presence.ClientStatus
-import com.discord.api.presence.ClientStatuses
 
 import de.robv.android.xposed.XC_MethodHook
 
 @AliucordPlugin(requiresRestart = false)
 internal class YAFNP : Plugin() {
     override fun start(context: Context) {
-        patcher.patch(Presence::class.java, arrayOf(ClientStatus::class.java, ClientStatuses::class.java, List::class.java), object : XC_MethodHook() {
+        patcher.patch(Presence::class.java.declaredConstructors[0], object : XC_MethodHook() {
             override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
                 val activities = param.args[2] as List<Activity>
                 for (activity in activities) {
